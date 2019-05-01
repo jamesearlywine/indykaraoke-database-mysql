5b4ea583aeee74301c34e0ad79ceecb464bab2ea
James Earlywine <james@esilogix.com>
2019-05-01 19:16:44 -0400

    ..

--- hosts/indykaraoke_localdev_down.sh
+++ hosts/indykaraoke_localdev_down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SCRIPT_DIRECTORY="${BASH_SOURCE[0]}"
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${SCRIPT_DIRECTORY}/_hosts.admin.sh"
 removeHost database-localdev.indykaraoke.com
--- hosts/indykaraoke_localdev_up.sh
+++ hosts/indykaraoke_localdev_up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-source ./_hosts.admin.sh
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "${SCRIPT_DIRECTORY}/_hosts.admin.sh"
 addHost 127.0.0.1 database-localdev.indykaraoke.com
